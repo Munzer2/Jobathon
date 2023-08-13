c4e8a82544d90d0b046d53b739e4103dbfe55010
Munzer2 <102376192+Munzer2@users.noreply.github.com>
2023-08-13 23:59:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const axios = require("axios");
 
 
 const express = require('express');
-const app = express();
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT; 
 
+const app = express();  
+
+app.use(express.static('public'));
 app.listen(3000);
 
-async function run(){
+
+async function run(query){
     const connection = await oracledb.getConnection({
         user: 'JOBATHON',
         password : '12345',
         connectString: 'localhost/orclpdb',
     });
 
+    const data = connection.execute(query); 
+
     await connection.close() ; //Always close connections.
+    return data; 
 }
 
 // run() ;
 
-app.get('/',(req,res)=> {
-    res.send(
-        {name :123, username : "nice"}
-    );  
+app.get('/user/:username', async(req,res)=> {
+    const username = req.params.username; 
+    const data = await run(`SELECT * FROM "Company" WHERE "Name"= '${username}'`); 
+    res.send(data.rows); 
+    res.end();    
 }); 
