08e9cfd91d26572484a8c5bae379938828841ea3
Munzer2 <102376192+Munzer2@users.noreply.github.com>
2023-08-24 11:46:39 +0600

    Added review table and populated user, employer, job seeker and review table at once.

--- Connect.js
+++ Connect.js
@@ -14,13 +14,13 @@ async function run(){
 
     const response = await axios.get('https://randomuser.me/api/?results=50');  
     ///For data population.
-    // console.log(response.data.results);   
+    ///console.log(response.data.results);   
     const CEO_Of_Company= ""; 
-    let c; 
-    let hobbies =["Reading manga","Football", "Books", "Gardening", "Cricket", "Table Tennis", "Long Tennis", "Organize", "Sleep", "Driving", "Cooking", "Games", "Streaming", "Eating", "Solitare", "Uno"]; 
+    let hobbies =["Reading manga","Football", "Books", "Gardening", "Cricket", "Table Tennis", "Long Tennis", "Organize", "Sleep", "Driving", "Cooking", "Games", "Streaming", "Eating", "Solitare", "Uno"];
+    let reviews = ["5","4","3","2","1"];  
     for(const person of response.data.results)
     {
-        c= Math.floor(Math.random()*3); 
+        let c= Math.floor(Math.random()*3); 
         let temp_cpID  = Math.floor(Math.random()*22) + 1;
         const cpID = temp_cpID;    
         const userID = person.login.uuid; 
@@ -33,21 +33,50 @@ async function run(){
         const gender = person.gender; 
         const Age = person.dob.age;
         const phone = person.phone; 
+        const pic_url_large = person.picture.large; 
+        const pic_url_med = person.picture.medium ; 
+        const pic_url_thumb =person.picture.thumbnail; 
         type =""; 
         if(c == 1) type = "Seeker";
-        else if(c == 0) type = "Employer"; 
+        else if(c == 0) type = "Employer";  
         await connection.execute(
         `INSERT INTO "User" 
-        VALUES('${userID}','${firstName}','${lastName}','${City}','${Postal}','${state}','${Country}','${Age}','${gender}','${phone}','${cpID}','${CEO_Of_Company}','${type}')`);
-        if(c == 0)
+        VALUES('${userID}','${firstName}','${lastName}','${City}','${Postal}','${state}','${Country}','${Age}','${gender}','${phone}','${cpID}','${CEO_Of_Company}','${type}','${pic_url_med}')`);
+        let idx = Math.floor(Math.random()*hobbies.length); 
+        await connection.execute(`INSERT INTO "Hobbies" VALUES('${userID}','${hobbies[idx]}')`);
+        if(c == 0)///Employer
         {
-            await connection.execute(`INSERT INTO "Employer" VALUES('${cpID}','${userID}')`); 
+            await connection.execute(`INSERT INTO "Employer" VALUES('${cpID}','${userID}','${pic_url_med}')`);
+            ///check whether he is a reviewer.
+            let check = Math.floor(Math.random()*2);
+            if(!check)
+            {
+                ///he is a reviewer.
+                let idx_review = Math.floor(Math.random()*reviews.length); 
+                await connection.execute(`INSERT INTO "Review" VALUES('${userID}','${cpID}','${reviews[idx_review]}')`); 
+            } 
         } 
-        else if(c == 1)
+        else if(c == 1)////Job seeker.
         {
-            let idx = Math.floor(Math.random()*hobbies.length); 
-            await connection.execute(`INSERT INTO "Job seeker" VALUES('${userID}')`);
-            await connection.execute(`INSERT INTO "Hobbies" VALUES('${userID}','${hobbies[idx]}')`);  
+            await connection.execute(`INSERT INTO "Job seeker" VALUES('${userID}','${pic_url_med}')`);
+            ///check whether he is a reviewer.  
+            let check = Math.floor(Math.random()*2);
+            if(!check)
+            {
+                ///he is a reviewer.
+                let idx_review = Math.floor(Math.random()*reviews.length); 
+                await connection.execute(`INSERT INTO "Review" VALUES('${userID}','${cpID}','${reviews[idx_review]}')`); 
+            } 
+        }
+        else///he may be currently working but provides reviews about his old companies. 
+        {
+            let check = Math.floor(Math.random()*2);
+            if(!check)
+            {
+                ///he is a reviewer.
+                let idx_review = Math.floor(Math.random()*reviews.length); 
+                await connection.execute(`INSERT INTO "Review" VALUES('${userID}','${cpID}','${reviews[idx_review]}')`); 
+            } 
         }
     }
     await connection.commit() ;
